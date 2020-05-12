b2912b35663102c279cd77fff1ee7ffb6fcc7ab4
hell_of_god <rinatoktaevich@gmail.com>
2020-05-12 22:16:08 +0300

    Changed function`s name. Added cleaning input function.

--- logic.js
+++ logic.js
@@ -1,13 +1,16 @@
-function insert (num){
+function addSymbol (num){
 document.formatto.textview.value=document.formatto.textview.value+num;
 }
 
 
-function endCut() {
+function cutValue() {
     let text=  document.formatto.textview.value;
     document.formatto.textview.value = text.slice(0,text.length-1);
 }
 
+function cleanInput(){
+    document.formatto.textview.value="";
+}
 
 function process() {
     let text=  document.formatto.textview.value;
