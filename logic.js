dd9dda87ee442cad55ec5f4051086883374ca001
hell_of_god <rinatoktaevich@gmail.com>
2020-05-12 23:36:51 +0300

    Made side menu toggle to btn click.

--- logic.js
+++ logic.js
@@ -1,18 +1,32 @@
-function addSymbol (num){
-document.formatto.textview.value=document.formatto.textview.value+num;
+function addSymbol(num) {
+    document.formatto.textview.value = document.formatto.textview.value + num;
 }
 
 
 function cutValue() {
-    let text=  document.formatto.textview.value;
-    document.formatto.textview.value = text.slice(0,text.length-1);
+    let text = document.formatto.textview.value;
+    document.formatto.textview.value = text.slice(0, text.length - 1);
 }
 
-function cleanInput(){
-    document.formatto.textview.value="";
+function cleanInput() {
+    document.formatto.textview.value = "";
 }
 
 function process() {
-    let text=  document.formatto.textview.value;
-    document.formatto.textview.value=eval(text);
+    let text = document.formatto.textview.value;
+    document.formatto.textview.value = eval(text);
+}
+
+
+function toggleSideMenu() {
+
+    let element = document.getElementById("sideMenu");
+
+
+    if (element.style.width === "0px") {
+        element.style.width = '52%';
+    } else {
+        element.style.width = '0px';
+    }
+
 }
