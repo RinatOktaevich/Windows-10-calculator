12a8c0326b045e7c7a41cf504d6cd6d5cf078bf7
hell_of_god <rinatoktaevich@gmail.com>
2020-05-15 05:01:44 +0300

    Implemented themes code for swithing from one to another.

--- logic.js
+++ logic.js
@@ -1,21 +1,10 @@
-function addSymbol(num) {
-    document.formatto.textview.value = document.formatto.textview.value + num;
-}
+(function () {
+    //Bunch of code...
+    applyTheme();
+})();
 
 
-function cutValue() {
-    let text = document.formatto.textview.value;
-    document.formatto.textview.value = text.slice(0, text.length - 1);
-}
 
-function cleanInput() {
-    document.formatto.textview.value = "";
-}
-
-function process() {
-    let text = document.formatto.textview.value;
-    document.formatto.textview.value = eval(text);
-}
 
 
 function toggleSideMenu() {
@@ -29,22 +18,52 @@ function toggleSideMenu() {
 }
 
 
-function setDarkTheme() {
-    let elements = document.getElementsByClassName("dark_theme");
-
+function applyTheme(theme) {
+    let elements = document.getElementsByClassName("colorAble");
+    let lastTheme = Cookies.get('theme') == undefined ? 'dark' : Cookies.get('theme');
     for (let index = 0; index < elements.length; index++) {
         const classList = elements[index].classList;
-        // classList.replace("light_theme", "dark_theme");
-        classList.remove("light_theme");
+        if (theme == undefined) {
+            classList.add(`${lastTheme}_theme`);
+        } else {
+            classList.replace(`${lastTheme}_theme`, `${theme}_theme`);
+        }
+    }
+
+    if (theme != undefined) {
+        Cookies.set('theme', theme);
+    } else {
+        Cookies.set('theme', lastTheme);
     }
 }
 
-function setLightTheme() {
-    let elements = document.getElementsByClassName("dark_theme");
+function lightTheme_btn_onclick() {
+    applyTheme('light');
+    toggleSideMenu();
+    Cookies.set('theme', 'light');
+}
 
-    for (let index = 0; index < elements.length; index++) {
-        const classList = elements[index].classList;
-        // classList.replace("dark_theme", "light_theme");
-        classList.add("light_theme");
-    }
+function darkTheme_btn_onclick() {
+    applyTheme('dark');
+    toggleSideMenu();
+    Cookies.set('theme', 'dark');
+}
+
+
+function addSymbol(num) {
+    document.formatto.textview.value = document.formatto.textview.value + num;
+}
+
+function cutValue() {
+    let text = document.formatto.textview.value;
+    document.formatto.textview.value = text.slice(0, text.length - 1);
+}
+
+function cleanInput() {
+    document.formatto.textview.value = "";
+}
+
+function process() {
+    let text = document.formatto.textview.value;
+    document.formatto.textview.value = eval(text);
 }
